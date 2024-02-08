7eb478496f2066d0fc9d7c2143c83e87b0146d62
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-02-08 20:35:34 +0530

    added method

--- app/src/main/java/com/hazrat/prayertimes/repository/PrayerTimeRepository.kt
+++ app/src/main/java/com/hazrat/prayertimes/repository/PrayerTimeRepository.kt
@@ -10,6 +10,7 @@ import com.hazrat.prayertimes.model.testmodel.MethodDetails
 import com.hazrat.prayertimes.network.PrayerTimeApi
 import com.hazrat.prayertimes.screen.UserSettingViewModel
 import com.hazrat.prayertimes.util.DateUtil
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 class PrayerTimeRepository @Inject constructor(
@@ -24,10 +25,11 @@ class PrayerTimeRepository @Inject constructor(
         val location: LocationEntity? = locationRepository.getLocation()
         val latitude = location?.latitude?: 0.0
         val longitude = location?.longitude?: 0.0
-        val method = methodRepository.getMethod()
-        Log.d("PrayerTimeRepository","$latitude $longitude $method")
+        val methodList = methodRepository.getMethod().firstOrNull() ?: emptyList()
+        val methodValue = methodList.firstOrNull()?.method ?: 1 // Default value is 1 if methodList or method is null
+        Log.d("PrayerTimeRepository","$latitude $longitude $methodValue")
         val currentDate = DateUtil.getCurrentDate()
-        return api.getPrayerTimes(currentDate, "$latitude", "$longitude", 1).data
+        return api.getPrayerTimes(currentDate, "$latitude", "$longitude", methodValue).data
     }
 
 }
